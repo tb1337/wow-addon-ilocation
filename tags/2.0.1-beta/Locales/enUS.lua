a5f427e242d2f91c45828bd91104311f1c8a9bc9
grdn <grdn@760fe7b6-452c-4466-99ec-6a44a9e872ef>
2012-07-12 07:51:42 +0000

    - Check revision 6

---
+++ tags/2.0.1-beta/Locales/enUS.lua
@@ -0,0 +1,26 @@
+local AddonName = ...;
+local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "enUS", true)
+
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
+
+--@do-not-package@
+L["By Difficulty"] = true
+L["By Hostility"] = true
+L["Encolor zone names"] = true
+L["Show dungeons in zone"] = true
+L["General Options"] = true
+L["Show recommended dungeons"] = true
+L["Show recommended zones"] = true
+L["Recommended Dungeons:"] = true
+L["Recommended Zones:"] = true
+L["Position:"] = true
+L["Sanctuary"] = true
+L["Contested"] = true
+L["Always enabled Levelmode"] = true
+L["Recommended zones or instances are only shown when you are not at maximum level. If enabled, they are always shown."] = true
+L["Decimal digits"] = true
+L["The number of decimal digits for coordinates."] = true
+L["Hide Raids"] = true
+L["Hide Dungeon Entrances"] = true
+L["Tooltip Options"] = true
+--@end-do-not-package@
