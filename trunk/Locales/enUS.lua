702eeb39538c7570c6ee6d5b898ab49e7a849632
grdn <grdn@760fe7b6-452c-4466-99ec-6a44a9e872ef>
2012-07-12 07:50:54 +0000

    - Overhaul of iLocation, embedded the new iLib - Removed LibQTip and AceAddon-3.0 as dependencies - Added AceHook-3.0 as dependency - iLocation calculates the coordination update time by itself - Many changes

--- trunk/Locales/enUS.lua
+++ trunk/Locales/enUS.lua
@@ -6,19 +6,21 @@ local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "enUS", true)
 --@do-not-package@
 L["By Difficulty"] = true
 L["By Hostility"] = true
-L["Encolor zone names by"] = true
+L["Encolor zone names"] = true
 L["Show dungeons in zone"] = true
-L["Tooltip Options"] = true
-L["Feed Options"] = true
+L["General Options"] = true
 L["Show recommended dungeons"] = true
 L["Show recommended zones"] = true
 L["Recommended Dungeons:"] = true
 L["Recommended Zones:"] = true
-L["Subzone:"] = true
-L["Zone:"] = true
+L["Position:"] = true
 L["Sanctuary"] = true
 L["Contested"] = true
 L["Always enabled Levelmode"] = true
-L["Hide Battlegrounds, Arenas and PvP-Zones"] = true
+L["Recommended zones or instances are only shown when you are not at maximum level. If enabled, they are always shown."] = true
+L["Decimal digits"] = true
+L["The number of decimal digits for coordinates."] = true
 L["Hide Raids"] = true
+L["Hide Dungeon Entrances"] = true
+L["Tooltip Options"] = true
 --@end-do-not-package@
--- trunk/iLocation.lua
+++ trunk/iLocation.lua
@@ -2,260 +2,332 @@
 -- Setting up scope and libs
 -----------------------------------
 
-local AddonName = select(1, ...);
-iLocation = LibStub("AceAddon-3.0"):NewAddon(AddonName, "AceEvent-3.0");
+local AddonName, iLocation = ...;
+LibStub("AceEvent-3.0"):Embed(iLocation);
+LibStub("AceTimer-3.0"):Embed(iLocation);
+LibStub("AceHook-3.0"):Embed(iLocation);
 
 local L = LibStub("AceLocale-3.0"):GetLocale(AddonName);
 
-local LibQTip = LibStub("LibQTip-1.0");
 local LibTourist = LibStub("LibTourist-3.0");
 
 local _G = _G;
 local format = _G.string.format; -- iLocation heavyly uses format!
 
+-------------------------------
+-- Registering with iLib
+-------------------------------
+
+LibStub("iLib"):Register(AddonName, nil, iLocation);
+
 -----------------------------------------
 -- Variables, functions and colors
 -----------------------------------------
 
-local Tooltip; -- our tooltip
-local CoordsTimer;
-
 local CurrentZone = "";
-local CurrentZoneColored = "";
 local CurrentSubZone = "";
 local CurrentPosX = 0;
 local CurrentPosY = 0;
 
+local CoordsTimer;
+
 local COLOR_GOLD = "|cfffed100%s|r";
 
------------------------------
--- Setting up the feed
------------------------------
+----------------------------
+-- Setting up the LDB
+----------------------------
 
-iLocation.Feed = LibStub("LibDataBroker-1.1"):NewDataObject(AddonName, {
+iLocation.ldb = LibStub("LibDataBroker-1.1"):NewDataObject(AddonName, {
 	type = "data source",
-	text = "iLocation",
-	--icon = "Interface\\Addons\\iLocation\\Images\\iGuild",
+	text = AddonName,
 });
 
-iLocation.Feed.OnClick = function(_, button)
-	if( button == "RightButton" ) then
+iLocation.ldb.OnClick = function(_, button)
+	if( button == "RightButton" and not _G.IsModifierKeyDown() ) then
 		iLocation:OpenOptions();
 	end
 end
 
-iLocation.Feed.OnEnter = function(anchor)
-	for k, v in LibQTip:IterateTooltips() do
-		if( type(k) == "string" and strsub(k, 1, 6) == "iSuite" ) then
-			v:Release(k);
-		end
+iLocation.ldb.OnEnter = function(anchor)
+	if( iLocation:IsTooltip("Main") ) then
+		return;
 	end
-		
-	Tooltip = LibQTip:Acquire("iSuite"..AddonName);
-	--Tooltip:SetAutoHideDelay(0.1, anchor);
-	Tooltip:SmartAnchorTo(anchor);
-	iLocation:UpdateTooltip();
-	Tooltip:Show();
+	iLocation:HideAllTooltips();
+	
+	local tip = iLocation:GetTooltip("Main", "UpdateTooltip");
+	tip:SmartAnchorTo(anchor);
+	tip:SetAutoHideDelay(0.1, anchor);
+	tip:Show();
 end
 
-iLocation.Feed.OnLeave = function()
-	Tooltip:Release();
-end
+iLocation.ldb.OnLeave = function() end
 
 ----------------------
 -- OnInitialize
 ----------------------
 
-function iLocation:OnInitialize()
+function iLocation:Boot()
 	self.db = LibStub("AceDB-3.0"):New("iLocationDB", self:CreateDB(), "Default").profile;
-	CoordsTimer = LibStub("AceTimer-3.0"):ScheduleRepeatingTimer(self.UpdateCoords, 1);
 	
-	self:RegisterEvent("PLAYER_ENTERING_WORLD", "UpdateZone");
-	self:RegisterEvent("ZONE_CHANGED_NEW_AREA", "UpdateZone");
-	self:RegisterEvent("ZONE_CHANGED", "UpdateZone");
+	self:RegisterEvent("ZONE_CHANGED", "EventHandler");
+	self:RegisterEvent("ZONE_CHANGED_NEW_AREA", "EventHandler");
 	
+	self:SecureHook("MoveBackwardStart", "StartMoving");
+	self:SecureHook("MoveBackwardStop", "StopMoving");
+	self:SecureHook("MoveForwardStart", "StartMoving");
+	self:SecureHook("MoveForwardStop", "StopMoving");
+	self:SecureHook("StrafeLeftStart", "StartMoving");
+	self:SecureHook("StrafeLeftStop", "StopMoving");
+	self:SecureHook("StrafeRightStart", "StartMoving");
+	self:SecureHook("StrafeRightStop", "StartMoving");
+	self:SecureHook("ToggleAutoRun", "StartOrStopAutoRun");
+	
+	self:EventHandler();
+	self:UpdateCoords();
+end
+iLocation:RegisterEvent("PLAYER_ENTERING_WORLD", "Boot");
+
+------------------------
+-- Moving Control
+------------------------
+
+local toggleAutoRun = false;
+
+function iLocation:StartOrStopAutoRun()
+	toggleAutoRun = not toggleAutoRun;
+	
+	if( toggleAutoRun ) then
+		self:StartMoving();
+	else
+		self:StopMoving();
+	end
+end
+
+do
+	local ratio, speed, speedwalk, speedfly;
+	
+	function iLocation:StartMoving()
+		if( CoordsTimer ) then
+			return;
+		end
+		
+		speed, speedwalk, speedfly = _G.GetUnitSpeed("player"); -- we determine our normal and flying speed
+		-- the more speed we have, the lesser is the result
+		-- this brings us as more coordinates updates as faster we are :)
+		speed = 1 / ((_G.IsFlying() and speedfly or speedwalk) / 7); -- since WoW speed is based on 7, we recalc it to base 10
+		ratio = 2 / (self.db.DecimalDigits + 1) * speed;
+		
+		CoordsTimer = self:ScheduleRepeatingTimer("UpdateCoords", ratio);
+	end
+end
+
+function iLocation:StopMoving()
+	if( not CoordsTimer ) then
+		return;
+	end
+	
+	self:CancelTimer(CoordsTimer);
+	CoordsTimer = nil;
+	toggleAutoRun = false;
+	self:UpdateCoords();
+end
+
+function iLocation:UpdateCoords()
+	CurrentPosX, CurrentPosY = _G.GetPlayerMapPosition("player");
+	iLocation:UpdatePlugin();
 end
 
 -----------------------
 -- Event Handling
 -----------------------
 
-function iLocation:UpdateZone()
+function iLocation:EventHandler()
 	CurrentZone = _G.GetRealZoneText() or _G.UNKNOWN;
 	CurrentZone = CurrentZone == "" and _G.UNKNOWN or CurrentZone;
 	
+	CurrentSubZone = _G.GetSubZoneText() or "";
+	
+	self:UpdatePlugin();
+end
+
+-----------------------
+-- Update Plugin
+-----------------------
+
+local function format_zone(zone, subzone)
+	if( not zone ) then
+		zone = CurrentZone;
+	end
+	subzone = subzone and CurrentSubZone == "" and false or subzone;
+	
 	local r, g, b;
+		
 	-- encolor by difficulty
-	if( self.db.ZoneColor == 2 ) then
-		r, g, b = LibTourist:GetLevelColor(CurrentZone);
-		CurrentZoneColored = ("|cff%02x%02x%02x%s|r"):format(r *255, g *255, b *255, CurrentZone);
-	elseif( self.db.ZoneColor == 3 ) then
-		r, g, b = LibTourist:GetFactionColor(CurrentZone);
-		CurrentZoneColored = ("|cff%02x%02x%02x%s|r"):format(r *255, g *255, b *255, CurrentZone);
+	if( iLocation.db.ZoneColor == 2 ) then
+		r, g, b = LibTourist:GetLevelColor(zone);
+		return ("|cff%02x%02x%02x%s|r"):format(r *255, g *255, b *255, zone..(subzone and ": "..CurrentSubZone or ""));
+	-- encolor by faction
+	elseif( iLocation.db.ZoneColor == 3 ) then
+		r, g, b = LibTourist:GetFactionColor(zone);
+		return ("|cff%02x%02x%02x%s|r"):format(r *255, g *255, b *255, zone..(subzone and ": "..CurrentSubZone or ""));
+	-- encolor gold
 	else
-		CurrentZoneColored = (COLOR_GOLD):format(CurrentZone);
+		return (COLOR_GOLD):format(zone..(subzone and ": "..CurrentSubZone or ""));
 	end
-	
-	CurrentSubZone = _G.GetSubZoneText() or _G.UNKNOWN;
-	CurrentSubZone = CurrentSubZone == "" and _G.UNKNOWN or CurrentSubZone;
-	
-	self:UpdateFeed();
 end
 
-function iLocation:UpdateCoords()
-	CurrentPosX, CurrentPosY = _G.GetPlayerMapPosition("player");
-	iLocation:UpdateFeed();
+local function format_coords(x, y)
+	if( not x ) then
+		x = CurrentPosX *100;
+	end
+	if( not y ) then
+		y = CurrentPosY *100;
+	end
+	
+	if( (x + y) == 0 ) then
+		return "";
+	end
+	return ("%."..iLocation.db.DecimalDigits.."f, %."..iLocation.db.DecimalDigits.."f"):format(x, y);
 end
 
------------------------
--- Update Feed
------------------------
+local function format_level(min, max, r, g, b)
+	return ("|cff%02x%02x%02x%s|r"):format(r *255, g *255, b *255, (min == max and min or ("%d-%d"):format(min, max)));
+end
 
-function iLocation:UpdateFeed()
-	self.Feed.text = CurrentZoneColored;
-	self.Feed.text = self.Feed.text.." "..("%.0f,%.0f"):format(CurrentPosX *100, CurrentPosY *100);
-	
-	if( LibQTip:IsAcquired("iSuite"..AddonName) ) then
-		self:UpdateTooltip();
-	end
+function iLocation:UpdatePlugin()
+	local coords = format_coords();
+	self.ldb.text = format_zone()..(coords ~= "" and " " or "")..coords;
+	self:CheckTooltips("Main");
 end
 
 -----------------------
 -- UpdateTooltip
 -----------------------
 
-local PVPStatus;
-do
-	local pvptype;
-	
-	PVPStatus = function()
-		pvptype = _G.GetZonePVPInfo();
+local function get_pvp_status()
+	local pvptype = _G.GetZonePVPInfo();
 		
-		if( pvptype == "arena" ) then
-			return _G.ARENA;
-		elseif( pvptype == "friendly" ) then
-			return _G.FRIENDLY;
-		elseif( pvptype == "contested" ) then
-			return L["Contested"];
-		elseif( pvptype == "hostile" ) then
-			return _G.HOSTILE;
-		elseif( pvptype == "sanctuary" ) then
-			return L["Sanctuary"];
-		elseif( pvptype == "combat" ) then
-			return _G.COMBAT;
-		else
-			return _G.UNKNOWN;
-		end
+	if( pvptype == "arena" ) then
+		return _G.ARENA;
+	elseif( pvptype == "friendly" ) then
+		return _G.FRIENDLY;
+	elseif( pvptype == "contested" ) then
+		return L["Contested"];
+	elseif( pvptype == "hostile" ) then
+		return _G.HOSTILE;
+	elseif( pvptype == "sanctuary" ) then
+		return L["Sanctuary"];
+	elseif( pvptype == "combat" ) then
+		return _G.COMBAT;
+	else
+		return _G.UNKNOWN;
 	end
 end
 
-local AddZone;
-do
-	local r, g, b, isPvP, isRaid;
-	
-	AddZone = function(zone)
-		isPvP = nil;
-		isRaid = nil;
+local function add_zone(tip, zone)
+	local r, g, b, min, max;
+	local size, entrance, x, y = "", "", 0, 0;
 		
-		-- check if it's PvP and filter it, if set in the options
-		if( LibTourist:IsPvPZone(zone) ) then
-			if( iLocation.db.HidePvPAreas ) then
-				return;
-			end
-			isPvP = true;
-		end
+	if( LibTourist:IsArena(zone) or LibTourist:IsBattleground(zone) or LibTourist:IsPvPZone(zone) ) then
+		return;
+	end
 		
-		if( LibTourist:IsArena(zone) ) then
-			if( iLocation.db.HideArenas ) then
-				return;
-			end
-			isPvP = true;
-		end
-		
-		if( LibTourist:IsBattleground(zone) ) then
-			if( iLocation.db.HideBattlegrounds ) then
-				return;
-			end
-			isPvP = true;
-		end
-		
-		-- check if it's a raid and filter it, if set in the options
-		-- if it's a PvP zone, we cancel the check for group size
-		if( not isPvP and LibTourist:GetInstanceGroupSize(zone) >= 10 ) then
+	if( LibTourist:IsInstance(zone) ) then
+		size = LibTourist:GetInstanceGroupSize(zone);
+		if( size >= 10 ) then
 			if( iLocation.db.HideRaids ) then
 				return;
 			end
-			isRaid = true;
+			size = "+";
+		else
+			size = "";
 		end
-		
-		r, g, b = LibTourist:GetLevelColor(zone);
-		mini, maxi = LibTourist:GetLevel(zone);
-		Tooltip:AddLine(zone, ("|cff%02x%02x%02x%s|r"):format(r *255, g *255, b *255,
-			(mini == maxi and mini or ("%d-%d"):format(mini, maxi))..
-			(isPvP and " |cffff0000".._G.PVP.."|r" or "")..
-			(isRaid and " |cffff4400".._G.RAID.."|r" or "")
-		));
+		entrance, x, y = LibTourist:GetEntrancePortalLocation(zone);
 	end
+		
+	r, g, b = LibTourist:GetLevelColor(zone);
+	min, max = LibTourist:GetLevel(zone);
+	
+	tip:AddLine(
+		zone, -- zone name
+		format_level(min, max, r, g, b), -- level req
+		(iLocation.db.HideRaids and nil or size),
+		(iLocation.db.HideEntrances and nil or format_zone(entrance or "")),
+		(iLocation.db.HideEntrances and nil or format_coords(x or 0, y or 0))
+	);
 end
 
-function iLocation:UpdateTooltip()
-	Tooltip:Clear();
-	Tooltip:SetColumnLayout(2, "LEFT", "LEFT");
+function iLocation:UpdateTooltip(tip)
+	tip:Clear();
+	tip:SetColumnLayout(
+		2 + (self.db.HideEntrances and 0 or 2) + (self.db.HideRaids and 0 or 1),
+		"LEFT", (self.db.HideRaids and "RIGHT" or "CENTER"), "CENTER", "LEFT", "RIGHT"
+	);
+	
+	local r, g, b, line;
 	
 	-- add zone name
-	local r, g, b = LibTourist:GetFactionColor(CurrentZone);
-	Tooltip:AddLine((COLOR_GOLD):format(L["Zone:"]), CurrentZoneColored.." "..("%.0f,%.0f"):format(CurrentPosX *100, CurrentPosY *100));
+	line = tip:AddLine("");
+	tip:SetCell(line, 1, format_zone(nil, true), nil, "CENTER", 0);
+	tip:AddLine("");
 	
-	-- sub zone may be unknown or empty string
-	if( CurrentSubZone ~= "" and CurrentSubZone ~= _G.UNKNOWN ) then
-		Tooltip:AddLine((COLOR_GOLD):format(L["Subzone:"]), CurrentSubZone);
+	-- add coordinates
+	local coords = format_coords();
+	if( coords ~= "" ) then
+		line = tip:AddLine((COLOR_GOLD):format(L["Position:"]));
+		tip:SetCell(line, 2, coords, nil, "RIGHT", 0);
 	end
 	
 	-- add pvp status
 	-- we need to check if it's a sanctuary, because Tourist encolors them yellow in some cases. I really dislike hardcoding.
-	if( PVPStatus() == L["Sanctuary"] ) then
+	r, g, b = LibTourist:GetFactionColor(CurrentZone);
+	if( get_pvp_status() == L["Sanctuary"] ) then
 		r, g, b = 0.41, 0.8, 0.94;
 	end
-	Tooltip:AddLine((COLOR_GOLD):format(_G.STATUS..":"), ("|cff%02x%02x%02x%s|r"):format(r *255, g *255, b *255, PVPStatus()));
+	line = tip:AddLine((COLOR_GOLD):format(_G.STATUS..":"));
+	tip:SetCell(line, 2, ("|cff%02x%02x%02x%s|r"):format(r *255, g *255, b *255, get_pvp_status()), nil, "RIGHT", 0);
 	
 	-- add level
-	local mini, maxi = LibTourist:GetLevel(CurrentZone);
-	if( mini > 0 and maxi > 0 ) then
+	local min, max = LibTourist:GetLevel(CurrentZone);
+	if( min > 0 and max > 0 ) then
 		r, g, b = LibTourist:GetLevelColor(CurrentZone);
-		Tooltip:AddLine((COLOR_GOLD):format(_G.LEVEL..":"), ("|cff%02x%02x%02x%s|r"):format(r *255, g *255, b *255, mini == maxi and mini or ("%d-%d"):format(mini, maxi)));
+		line = tip:AddLine((COLOR_GOLD):format(_G.LEVEL..":"));
+		tip:SetCell(line, 2, format_level(min, max, r, g, b), nil, "RIGHT", 0);
 	end
 	
 	-- add continent
-	Tooltip:AddLine((COLOR_GOLD):format(_G.CONTINENT..":"), LibTourist:GetContinent(CurrentZone));
+	line = tip:AddLine((COLOR_GOLD):format(_G.CONTINENT..":"));
+	tip:SetCell(line, 2, LibTourist:GetContinent(CurrentZone), nil, "RIGHT", 0);
 	
 	-- add existing instances in a zone
 	if( self.db.ShowZoneInstances and LibTourist:DoesZoneHaveInstances(CurrentZone) ) then
-		Tooltip:AddLine(" ");
-		Tooltip:AddLine((COLOR_GOLD):format(_G.DUNGEONS..":"));
+		tip:AddLine(" ");
+		tip:AddLine((COLOR_GOLD):format(_G.DUNGEONS..":"));
 		
 		for zone in LibTourist:IterateZoneInstances(CurrentZone) do
-			AddZone(zone);
+			add_zone(tip, zone);
 		end
 	end
 	
 	-- if not max level, show recommended zones
-	if( self.db.AlwaysLevelmode or UnitLevel("player") < MAX_PLAYER_LEVEL ) then
+	if( self.db.AlwaysLevelmode or _G.UnitLevel("player") < _G.MAX_PLAYER_LEVEL ) then
 		if( self.db.ShowRecZones ) then
-			Tooltip:AddLine(" ");
-			Tooltip:AddLine((COLOR_GOLD):format(L["Recommended Zones:"]));
+			tip:AddLine(" ");
+			tip:AddLine((COLOR_GOLD):format(L["Recommended Zones:"]));
 			
 			for zone in LibTourist:IterateRecommendedZones() do
-				AddZone(zone);
+				add_zone(tip, zone);
 			end
 		end
 		
 		if( self.db.ShowRecInstances ) then
-			Tooltip:AddLine(" ");
-			Tooltip:AddLine((COLOR_GOLD):format(L["Recommended Dungeons:"]));
+			tip:AddLine(" ");
+			tip:AddLine((COLOR_GOLD):format(L["Recommended Dungeons:"]));
 			
 			for zone in LibTourist:IterateRecommendedInstances() do
-				AddZone(zone);
+				add_zone(tip, zone);
 			end
 		end
 	end
+	
 end
